639ad76d6e38049e4da7cbba6bde86dcfae469c9
kxs <1814894165@qq.com>
2020-04-20 09:53:51 +0800

    增加高级模板

---
+++ ask.js
@@ -0,0 +1,33 @@
+module.exports = [
+	{
+        type: 'input',
+        name: 'name',
+        message: 'project name?',
+    },
+	{
+        type: 'input',
+        name: 'version',
+        message: 'project version?',
+    },
+    {
+        type: 'confirm',
+        name: 'private',
+        message: 'ths resgistery is private?',
+    },
+    {
+        type: 'input',
+        name: 'author',
+        message: 'author?',
+    }, 
+    {
+        type: 'input',
+        name: 'description',
+        message: 'description?',
+    }, 
+    {
+        type: 'input',
+        name: 'license',
+        message: 'license?',
+    }
+
+];
